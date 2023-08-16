4298edbbc0e4c4167b1817a56e3c0076ef3cd022
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-16 15:59:49 -0600

    add @nestjs/mongoose mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { PokemonModule } from './pokemon/pokemon.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),

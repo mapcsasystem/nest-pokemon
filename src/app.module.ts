8965265fa7cc4ee07eb5c814cad84df6af36fc5b
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-16 14:40:45 -0600

    server static && ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
 })
 export class AppModule {}

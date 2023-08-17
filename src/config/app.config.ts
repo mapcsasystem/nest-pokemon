dbb8065f3d6bc8c66392cc70130cb73ef641b666
Miguel Angel Pérez Contreras <mapcsasystem@gmail.com>
2023-08-17 15:47:56 -0600

    envConfiguration  PORT: +process.env.PORT

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,5 +1,5 @@
 export const envConfiguration = () => ({
   NODE_ENV: process.env.NODE_ENV || 'dev',
   MONGO_DB_URI: process.env.MONGO_DB_URI,
-  PORT: process.env.PORT || 3000,
+  PORT: +process.env.PORT || 3000,
 });
